e25d2b9c87be5ac1d68c2a8fda26826dc5b90488
thefallnn <thefallnn@gmail.com>
2023-03-21 11:54:32 +0530

    refactor enquiry portal

--- services.php
+++ services.php
@@ -6,16 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 </head>
 
 <h1>Services Available </h1>
+<a style="margin-left:62%; margin-top:-70px;" class='btn btn-primary mb-4' href='service_form.php'>Add Service</a>
+<a style="margin-left:70%; margin-top:-119px; background-color:#04aa6d; border-color:#04aa6d;" class='btn btn-primary mb-4' href='index.php'>Go Back</a>
 
 <body style="margin: 10px; ">
     <br>
     <table class="table">
+        
         <thead>
             <tr>
                 <th>service_type_id</th>
@@ -54,7 +56,7 @@
                 echo "<tr>
                     <td>" . $row["service_type_id"] . "</td>
                     <td>" . $row["service_type_name"] . "</td>
-                    <td>" . $row["status"] . "</td>
+                    <td> </td>
                     <td>" . $row["priority"] . "</td>
                     <td>
                         <a class='btn btn-primary btn-sm' href='update'>Update</a>
@@ -68,6 +70,6 @@
         </tbody>
     </table>
 </body>
-<button type="button" class="btn btn-primary" onclick="window.location.href='service_form.php';">Add Service</button>
+<!-- <button type="button" class="btn btn-primary" onclick="window.location.href='service_form.php';">Add Service</button> -->
 
 </html>
