7a8689c7e8f618f7a35b02d96e77d750525130f5
thefallnn <thefallnn@gmail.com>
2023-03-22 18:18:01 +0530

    service-form: refactor code mapping

--- enquiry_form.php
+++ enquiry_form.php
@@ -1,4 +1,3 @@
-<?php include 'souravx.php'; ?>
 <!doctype html>
 <html lang="en">
 
--- service_form.php
+++ service_form.php
@@ -49,75 +49,75 @@
 													<input type="text" class="form-control" name="service_type_name" id="service_type_name" placeholder="Enter Service Name">
 												</div>
 											</div>
-											<div class="col-md-12">
-												<div class="form-group">
-													<div class="row form-group ">
+					<div class="col-md-12">
+						<div class="form-group">
+							<div class="row form-group ">
 
-														<label class="form-check-label ml-3 mb-5" for="priority"> Service Priority</label>
-														<div class="form-check form-check-inline ">
-															<input class="form-control ml-3 mb-5" type="text" name="priority" id="priority" value="">
-															<label class="form-check-label" for="priority"></label>
-														</div>
-
-
+								<label class="form-check-label ml-3 mb-5" for="priority"> Service Priority</label>
+								<div class="form-check form-check-inline ">
+									<input class="form-control ml-3 mb-5" type="text" name="priority" id="priority" value="">
+									<label class="form-check-label" for="priority"></label>
+								</div>
 
 
 
-													</div>
 
-												</div>
-											</div>
-											<div class="col-md-12">
-												<div class="form-group">
-													<input type="submit" value="Submit" class="btn btn-primary">
-													<div class="submitting"></div>
-												</div>
-											</div>
+
+					</div>
+
 										</div>
-									</form>
+									</div>
+									<div class="col-md-12">
+										<div class="form-group">
+											<input type="submit" value="Submit" class="btn btn-primary">
+											<div class="submitting"></div>
+										</div>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+			<div class="col-lg-4 col-md-5 d-flex align-items-stretch">
+						<div class="info-wrap bg-primary w-100 p-md-5 p-4">
+							<h3>Service Portal</h3>
+							<p class="mb-4">service enquiry and submission portal for metamint clients</p>
+							<div class="dbox w-100 d-flex align-items-start">
+								<div class="icon d-flex align-items-center justify-content-center">
+									<span class="fa fa-map-marker"></span>
+								</div>
+								<div class="text pl-3">
+									<p><span>Address:</span> Shibnagar, Modern Club Chowmuhani,, Agartala, Tripura.</p>
 								</div>
 							</div>
-							<div class="col-lg-4 col-md-5 d-flex align-items-stretch">
-								<div class="info-wrap bg-primary w-100 p-md-5 p-4">
-									<h3>Service Portal</h3>
-									<p class="mb-4">service enquiry and submission portal for metamint clients</p>
-									<div class="dbox w-100 d-flex align-items-start">
-										<div class="icon d-flex align-items-center justify-content-center">
-											<span class="fa fa-map-marker"></span>
-										</div>
-										<div class="text pl-3">
-											<p><span>Address:</span> Shibnagar, Modern Club Chowmuhani,, Agartala, Tripura.</p>
-										</div>
-									</div>
-									<div class="dbox w-100 d-flex align-items-center">
-										<div class="icon d-flex align-items-center justify-content-center">
-											<span class="fa fa-phone"></span>
-										</div>
-										<div class="text pl-3">
-											<p><span>Phone:</span> <a href="tel://9366744376"> +91 9366744376</a></p>
-										</div>
-									</div>
-									<div class="dbox w-100 d-flex align-items-center">
-										<div class="icon d-flex align-items-center justify-content-center">
-											<span class="fa fa-paper-plane"></span>
-										</div>
-										<div class="text pl-3">
-											<p><span>Email:</span> <a href="mailto:info@yoursite.com">info@metamintindia.com</a></p>
-										</div>
-									</div>
-									<div class="dbox w-100 d-flex align-items-center">
-										<div class="icon d-flex align-items-center justify-content-center">
-											<span class="fa fa-globe"></span>
-										</div>
-										<div class="text pl-3">
-											<p><span>Website</span> <a href="https://www.metamintindia.com/">www.metamintindia.com</a></p>
-										</div>
-									</div>
+							<div class="dbox w-100 d-flex align-items-center">
+								<div class="icon d-flex align-items-center justify-content-center">
+									<span class="fa fa-phone"></span>
+								</div>
+								<div class="text pl-3">
+									<p><span>Phone:</span> <a href="tel://9366744376"> +91 9366744376</a></p>
+								</div>
+							</div>
+							<div class="dbox w-100 d-flex align-items-center">
+								<div class="icon d-flex align-items-center justify-content-center">
+									<span class="fa fa-paper-plane"></span>
+								</div>
+								<div class="text pl-3">
+									<p><span>Email:</span> <a href="mailto:info@yoursite.com">info@metamintindia.com</a></p>
+								</div>
+							</div>
+							<div class="dbox w-100 d-flex align-items-center">
+								<div class="icon d-flex align-items-center justify-content-center">
+									<span class="fa fa-globe"></span>
+								</div>
+								<div class="text pl-3">
+									<p><span>Website</span> <a href="https://www.metamintindia.com/">www.metamintindia.com</a></p>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
+			</div>
+		</div>
 
 			</div>
 		</div>
