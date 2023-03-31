8e2faa27cb3adf7f6d83d26949daae2ce0e74b50
thefallnn <thefallnn@gmail.com>
2023-03-31 16:56:27 +0530

    add initial footer and a nav blueprint

---
+++ resources/footer.php
@@ -0,0 +1,75 @@
+<!-- Footer -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<footer class="text-center text-lg-start bg-white text-muted">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <!-- Left -->
+
+        <!-- Left -->
+
+        <!-- Right -->
+
+        <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="">
+        <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <i class="fas fa-gem me-3 text-secondary"></i>Meta Mint India
+                    </h6>
+                    <p>
+                        a software development company
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <i class="fas fa-phone me-3 text-secondary"></i>Contact
+                    </h6>
+                    <p><i class=" text-uppercase fw-bold mb-4"></i>+91 9366744376</p>
+
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <i class="fas fa-home me-3 text-secondary"></i>Location
+                    </h6>
+                    <p><i class=" text-uppercase fw-bold mb-4"></i> Agartala, Tripura.</p>
+
+
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.025);">
+        © <?php $year = date("Y");
+            echo $year; ?> Copyright:
+        <a class="text-reset fw-bold" href="https://metamintindia.com/">META MINT</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer -->
